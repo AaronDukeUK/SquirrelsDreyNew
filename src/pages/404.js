6a6e7eb978c4b402dc473314f582253772b1e447
Aaron Duke <aaronduke22@gmail.com>
2020-08-10 21:11:32 +0100

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,51 @@
-import React from "react"
-import { SEO } from "../components"
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+import Img from 'gatsby-image/withIEPolyfill'
+
+import { Layout } from '../layouts'
+import { SEO } from '../components'
+
+const getFound = graphql`
+  {
+    notes: file(name: { eq: "clover" }) {
+      name
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+const NotFoundPage = () => {
+  const data = useStaticQuery(getFound)
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <main className="aboutContainer">
+        <section className="aboutSectionContainer greyBG">
+          <div className="aboutSection">
+            <div className="aboutText">
+              <h2>NOT FOUND</h2>
+              <p className="lead">
+                You just hit a route that doesn&#39;t exist.
+              </p>
+              <p>Are you feeling Lucky?</p>
+            </div>
+            <div className="aboutImage">
+              <Img
+                alt={data.notes.name}
+                fluid={data.notes.childImageSharp.fluid}
+                objectFit="cover"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
+  )
+}
 
 export default NotFoundPage

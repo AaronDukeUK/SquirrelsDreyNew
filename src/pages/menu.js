c683f80db999ee139bbaf303d1b88714b10286b1
Aaron Duke <aaronduke22@gmail.com>
2021-07-28 13:31:15 +0100

    menu

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -95,7 +95,7 @@ const Menu = () => {
                 <p>Starters</p>
                 <p>Toasted handmade seasalt and rosemary focaccia, herb pitted olives, dipping oil | £6</p>
                 <p>Liquorice and beet cured salmon, citrus crème fraiche, pickled fennel, broad beans | GF | £8</p>
-                <p>Two-way rabbit, thyme hash, kale crisps, spiced pear and port relish | GF | £9</p>
+                <p>Rabbit loin, roasted jersey royals, pickled blackberries, quail scotch egg | £8</p>
                 <p>Ham hock bonbons, smoked cheddar tart, caramelised onion puree, cider and sage jelly | £8.50</p>
               </div>
               <div>
@@ -103,7 +103,7 @@ const Menu = () => {
                 <p>Lamb breast, pistachio and fig, rosemary cauliflower, potato harissa scone, romesco sauce | N GF | £18</p>
                 <p>Pork belly, Garden beans, smoked lardons, fondant potato, bramley puree | GF | £17.50</p>
                 <p>Braised brisket, cep and stilton wellington, grandpa’s cabbage, smoked celeriac croquettes, port jus | £19</p>
-                <p>Sea Bream fillet, celeriac dauphinoise, stem broccoli, tomato ravioli, smoked almonds | N GF | £18</p>
+                <p>Sea Bream fillet, celeriac dauphinoise, sweet peas, tomato ravioli, smoked almonds, bouillabaisse sauce | N GF | £18</p>
                 <p>Beef and ale pie, Roast jersey potatoes, heritage thyme carrots, onion puree | £15</p>
                 <p>Cep and smoked ricotta tortellini, sage crisps, garden beans, cashew nut butter | N V | £16</p>
               </div>
